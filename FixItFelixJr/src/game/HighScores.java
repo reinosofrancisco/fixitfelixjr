03ab65d4f8f3c356ef18f8938961f26b47edebb7
reinosofrancisco <reinosofrancisco_@hotmail.com>
2019-10-28 17:42:14 -0300

    scores altos
    altos scores

--- FixItFelixJr/src/game/HighScores.java
+++ FixItFelixJr/src/game/HighScores.java
@@ -1,14 +1,38 @@
 package game;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class HighScores {
-	private SortedSet<Scores> s = new TreeSet<Scores>();
+	private SortedSet<Scores> lovelyScores = new TreeSet<Scores>();
 	
 	private static HighScores INSTANCE;
 	
+	@SuppressWarnings("unchecked")
 	private HighScores() {
+		ObjectInputStream imput = null;
+		
+		try {	
+			//Asigno la Entrada, de donde voy a sacar los datos
+			 imput = new ObjectInputStream(new FileInputStream("C:\\DownloadJr\\test.dat"));	
+			 //Saco el objeto que guarde anteriormente
+			 this.lovelyScores = (SortedSet<Scores>) imput.readObject();			
+		} 
+		catch (FileNotFoundException ex) {
+			System.out.println(ex.getMessage());
+		}catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		} finally {//TODO }
+			/**Luego de un error, se ejecutara el codigo que esta en el Finally. Luego, el programa SIGUE. */
+		}
 		
 	}
 	
@@ -20,9 +44,35 @@ public class HighScores {
 	}
 	
 	public void add(String name, int puntos) {
-		s.add(new Scores(name, puntos));
-		if(s.size()>5) {
-			s.remove(s.first());
+		lovelyScores.add(new Scores(name, puntos));
+		if(lovelyScores.size()>5) {
+			lovelyScores.remove(lovelyScores.first());
+		}
+	}
+	
+	public void printHightScores() {
+		for (Scores scores : lovelyScores) {
+			System.out.println("Score! --> " + scores);
+		}
+	}
+	
+	/**Saves scores to a binary file */
+	public void savePersistentScore() {
+		ObjectOutputStream output = null;	
+		try {	
+			//Asigno a salida donde voy a guardar los datos
+			output = new ObjectOutputStream(new FileOutputStream("C:\\DownloadJr\\test.dat"));
+			//Guardo los hightscores en el archivo (SortedSet implements Serializable)
+			output.writeObject(lovelyScores);
+		} 
+		catch (FileNotFoundException ex) {
+			System.out.println(ex.getMessage());
+		}catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		} finally {//TODO }
+			/**Luego de un error, se ejecutara el codigo que esta en el Finally. Luego, el programa SIGUE. */
 		}
 	}
 	
