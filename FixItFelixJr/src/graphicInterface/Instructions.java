6942d728dc9d2da5c4e7a7a91e7d0cfa30e348a6
reinosofrancisco <reinosofrancisco_@hotmail.com>
2019-10-31 16:57:53 -0300

    aa

--- FixItFelixJr/src/graphicInterface/Instructions.java
+++ FixItFelixJr/src/graphicInterface/Instructions.java
@@ -20,7 +20,7 @@ public class Instructions extends JPanel{
 		 * 
 		 */
 		private static final long serialVersionUID = 1L;
-		private String imageUrl= "graphicInterface/Image/siberian-huskies-900.jpg";
+		private String imageUrl= "graphicInterface/Image/fixItFelix.jpg";
 		private Image img;
 		
 		public Instructions()
