2359822fc97b8aac3dc6aa1184e2a9426b72fc14
JeroAlfonsin <jeronimoalfonsin@gmail.com>
2019-11-14 16:42:08 -0300

    aa

--- FixItFelixJr/src/guiControllers/MouseContInputHS.java
+++ FixItFelixJr/src/guiControllers/MouseContInputHS.java
@@ -2,8 +2,6 @@ package guiControllers;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
-import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
 import game.Core;
@@ -24,7 +22,8 @@ public class MouseContInputHS extends MouseAdapter
 		String name=InputScorePanel.getInstance().getArea1().getText();
 		if(name.length() <= 2 || name.contains(" ") || HighScores.getInstance().getScoreList().contains(new Scores(name, Core.getInstance().getPoints())))
 		{
-			JOptionPane.showConfirmDialog(new JButton(), "No es un nombre valido", "ERROR", JOptionPane.YES_OPTION, JOptionPane.WARNING_MESSAGE);		}
+			JOptionPane.showMessageDialog(null, "No es un nombre valido", "ERROR", JOptionPane.ERROR_MESSAGE);
+		}
 		else
 		{
 			if(name.length()>20)
